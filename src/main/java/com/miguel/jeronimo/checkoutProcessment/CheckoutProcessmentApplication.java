2affac493f007bd3f3c438fcec52daacab298aff
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2025-04-11 19:43:59 -0300

    initial commit

---
+++ src/main/java/com/miguel/jeronimo/checkoutProcessment/CheckoutProcessmentApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.jeronimo.checkoutProcessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutProcessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutProcessmentApplication.class, args);
+	}
+
+}
